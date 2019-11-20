96fc089bd03df3759f305ddad876e6cfeadc8eea
Christian Owen <cowen314@gmail.com>
2019-11-19 21:29:58 -0600

    remove old buffer code

--- source/engines/data_buffer_engine.py
+++ source/engines/data_buffer_engine.py
@@ -38,29 +38,3 @@ class BufferEngine(_BufferEngine):
         subscription_queue = SimpleQueue()
         self._subscriptions[key].append(subscription_queue)
         return subscription_queue
-
-
-class _InputHandler:
-    @abstractmethod
-    def process_input(self):
-        pass
-
-# TODO define concrete implementations of the above class
-
-
-class DefaultInputHandler(_InputHandler):
-    def __init__(self):
-        pass
-
-    def process_input(self):
-        pass
-
-
-class DataBufferEngine:
-    def __init__(self, message_center: _MessageCenter = PubSubMessageCenter()):
-        self._message_center = message_center
-        self._input_handlers = []
-
-    def add_input(self, topic: str, frames_to_buffer: int):
-        # self._message_center.subscribe(self.process_input, )
-        pass
