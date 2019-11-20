a76e37cd5c94aca5e38eaca7673e6f3a6c0af2db
Christian Owen <cowen314@gmail.com>
2019-11-19 21:28:56 -0600

    complete implementation of simple buffer engine

--- source/engines/data_buffer_engine.py
+++ source/engines/data_buffer_engine.py
@@ -1,6 +1,7 @@
 from ..messaging import _MessageCenter, PubSubMessageCenter
 from abc import abstractmethod
 from queue import SimpleQueue
+from typing import List, Dict, Any
 
 
 class _BufferEngine(object):
@@ -18,8 +19,25 @@ class _BufferEngine(object):
 
 
 class BufferEngine(_BufferEngine):
+    def __init__(self):
+        self._subscriptions: Dict[str, List[SimpleQueue]] = {}
+        self._values: Dict[str, Any] = {}
+
     def read_latest_value(self, key: str):
-        pass
+        return self._values[key]
+
+    def write(self, key: str, value, synchronous: bool = False):
+        self._values[key] = value
+        if key in self._subscriptions:
+            for subscription_queue in self._subscriptions[key]:
+                subscription_queue.put(value, block=False)
+
+    def create_subscription(self, key: str) -> SimpleQueue:
+        if key not in self._subscriptions:
+            self._subscriptions[key] = []
+        subscription_queue = SimpleQueue()
+        self._subscriptions[key].append(subscription_queue)
+        return subscription_queue
 
 
 class _InputHandler:
