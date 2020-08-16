3eb2355884f56a46ef4794a779b225b40816e202
Christian Owen <cowen314@gmail.com>
2020-08-16 15:19:42 -0500

    add eslint and config, start updating code to match style recommendations; add custom websocket hook

--- source/ui/web/react-app/src/components/HomeTab.js
+++ source/ui/web/react-app/src/components/HomeTab.js
@@ -1,100 +1,106 @@
-import React, { Component, useState, useEffect } from 'react';
-import io from 'socket.io-client';
+/* eslint-disable no-useless-constructor */
+import React, { Component, useState, useEffect } from 'react'
+import io from 'socket.io-client'
 
 class HomeTab extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.state = {date: new Date(), count: 0};
-    }
+  constructor (props) {
+    super(props)
+    // this.state = {date: new Date(), count: 0};
+  }
 
-    
+  render(props) {
+    return <>
+      <div>
+        <DateAndCounter />
+      </div>
+      <div>
+        <ValueTable />
+        <AAdder />
+        <SigleValue socket={io('127.0.0.1:5001')}/> //
+      </div>
 
-    render(props) { 
-        return <>
-            <div>
-                <DateAndCounter />
-            </div>
-            <div>
-                <ValueTable />
-                <AAdder />
-                <SigleValue socket={io("127.0.0.1:5001")}/> //
-            </div>
-
-        </>;
-    };
+    </>
+  };
 };
 
 
 class ValueTable extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    // How do I generate HTML programatically?
-    // Eventually, populate the table with whatever initial variables and values are passed in to the constructor
-        // on ComponentDidMount() of the HomeTab component, fetch all available keys  
+  // How do I generate HTML programatically?
+  // Eventually, populate the table with whatever initial variables and values are passed in to the constructor
+  // on ComponentDidMount() of the HomeTab component, fetch all available keys  
 
-    render(props) {
-        return <>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Value</td>
-                    </tr>
-                </thead>
-            </table>
-        </>;
-    }
+  render (props) {
+    return <>
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Value</td>
+          </tr>
+        </thead>
+      </table>
+    </>;
+  }
 }
 
-
-function AAdder(){
-    const [text, setText] = useState("initial");  // returns a state "thing" (variable?) and a function to set that thing 
-    return <>
-    <button onClick={() => setText(text+"a")}>Test</button>
+function AAdder() {
+  const [text, setText] = useState('initial');  // returns a state "thing" (variable?) and a function to set that thing 
+  return <>
+    <button onClick={() => setText(text+'a')}>Test</button>
     <p>{text}</p>
-    </>
+  </>
 }
 
 
 function SingleValue(props) {
-    const [value, setValue] = useState(-1);
-    useEffect({
-        // a function that subscribes this component to updates pushed over the web socket
-        props.socket.connect("");
-    })
-    return <>
-        <p>{value}</p>
-    </>
+  const value = useNumericValue(props.key, props.socket);
+  return <>
+    <p>{value}</p>
+  </>
+}
+
+
+function useNumericValue(key, socket) {
+  const [value, setValue] = useState(-1);
+
+  useEffect(() => {
+    // check to see if a connection already exists
+    socket.on(key, (value) => setValue(value));
+    // maybe return a function to cleanup the connection?
+  });
+  return value;
 }
 
 
 class DateAndCounter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date(), count: 0};
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date(), count: 0};
 
-        // be sure to bind callbacks to the class! (this lets you refer to them as 'this.<method_name>')
-        this.handleButtonPress = this.handleButtonPress.bind(this);
-    }
+    // be sure to bind callbacks to the class! (this lets you refer to them as 'this.<method_name>')
+    this.handleButtonPress = this.handleButtonPress.bind(this);
+  }
 
-    handleButtonPress() {
-        this.setState(state => ({
-            date : state.date,
-            count : ++state.count
-        }));
-    }
+  handleButtonPress() {
+    this.setState(state => ({
+      date : state.date,
+      count : ++state.count
+    }));
+  }
     
-    render(props) { 
-        return <>
-        <p>
+  render(props) { 
+    return <>
+      <p>
             The date is {this.state.date.toDateString()} <br/>
             The button has been pressed {this.state.count} times
-        </p>
-        <button onClick={this.handleButtonPress}>Press here</button>
-        </>;
-    };
+      </p>
+      <button onClick={this.handleButtonPress}>Press here</button>
+    </>;
+  };
 }
 
 export default HomeTab;
