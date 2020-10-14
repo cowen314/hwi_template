ee963451422a3e1f2a53c0f7638a56bd680965d1
Christian Owen <cowen314@gmail.com>
2020-10-13 20:24:11 -0500

    link socket into graph component, change composition

--- source/ui/web/react-app/src/components/DataPipeline.tsx
+++ source/ui/web/react-app/src/components/DataPipeline.tsx
@@ -5,14 +5,104 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { IconButton } from '@material-ui/core';
+import { Container, IconButton, makeStyles, useTheme } from '@material-ui/core';
 import { SocketType } from '../custom_type_declarations/common-types';
+import { ResponsiveContainer, LineChart, XAxis, YAxis, Label, Line } from 'recharts';
+import Title from '../dashboard/Title';
+import io from 'socket.io-client';
+// import { useStyles } from '../dashboard/Dashboard';
 
 // Goal: determine how to best pass frequently updating data/tags to React components,
 // where the components need to render a variable number of tags (e.g. in a dynamic list, graph, etc) 
 
 // Option 1: using composition
 // Pass the data in as props
+
+const drawerWidth = 240;
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    toolbar: {
+      paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    menuButtonHidden: {
+      display: 'none',
+    },
+    title: {
+      flexGrow: 1,
+    },
+    drawerPaper: {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerPaperClose: {
+      overflowX: 'hidden',
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9),
+      },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+      backgroundColor:
+        // theme.palette.mode === 'light'
+        //   ? theme.palette.grey[100]
+        //   : theme.palette.grey[900],
+        theme.palette.grey[100],
+      flexGrow: 1,
+      height: '100vh',
+      overflow: 'auto',
+    },
+    container: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      display: 'flex',
+      overflow: 'auto',
+      flexDirection: 'column',
+    },
+    fixedHeight: {
+      height: 240,
+    },
+}));
 
 function ValueTable(props : {tags : any}){  // FIXME
     return <>
@@ -54,36 +144,48 @@ function ValueTable(props : {tags : any}){  // FIXME
     </>
 }
 
-function updateBuffer() {
-
-}
-
-function DataBuffers(props: {socket: SocketType}, socketEventName: string, lengths: BigInteger) {
+function BufferedGraph(props: {socket: SocketType, socketEventName: string, lengths: number}) {
+    // TODO move the buffering logic to another component or entity that can be reused in other places where buffering is needed
     const [bufferedData, setBufferedData] = useState({}); // dictionary(string, number[]))
-    // LEFT OFF HERE: when something happens (new tags data received), we need to update the buffers
-    // const newDataReceived = () => {
-
-    // };
-    useEffect(props.socket.on(socketEventName, (data: any) => {  // FIXME switch the type of `data`
-        // concatenate the values in the dictionary with existing values in the buffer, removing old samples
-        Object.keys(data).forEach((key) => (bufferedData as any)[key].concatenate(data[key]));  // TODO rotate out any samples beyond given length
-        setBufferedData(bufferedData);
+    useEffect(
+        () => props.socket.on(props.socketEventName, (data: any) => {  // FIXME switch the type of `data`
+            // concatenate the values in the dictionary with existing values in the buffer, removing old samples
+            Object.keys(data).forEach((key) => (bufferedData as any)[key].concatenate(data[key]));  // TODO rotate out any samples beyond given length
+            setBufferedData(bufferedData);
     }));
+    return <>
+    <Graph data={bufferedData}/>
+    </>
 }
 
-function Graph(props: {dataBuffers: JsxElement}) {
+// function DataBuffer(props: {socket: SocketType, socketEventName: string, lengths: BigInteger}) {
+
+// }
+
+function Graph(props: {data: any}) {
+    const theme = useTheme();
     // store the current values in buffers (part of this component's state)
     // when something happens, the buffers here get updated
     // don't think this can be compositional, because it actually has state
     // we _could_ pass a buffer component into this as a prop... yeah, let's do that
 
     // OK so I think?? I might need to use context here?
-    // LEFT OFF HERE: It seems like I need to "subscribe" to rerenders of the DataBuffers... 
+    
+    // Option 1: use context
+    // I would create a context,
+    // wrap the graph object in that context,
+    // then implement the graph so that it pulls from the context.
+    // 
+
+    // Option 2: pass the databuffers piece of the state of the DataBuffers into this class
+    // is this possible without nesting this component directly in the data buffers component?
+    // could maybe get around direct nesting with generics
+
     return <React.Fragment>
         <Title>Today</Title>
         <ResponsiveContainer>
           <LineChart
-            data={data}
+            data={props.data}
             margin={{
               top: 16,
               right: 16,
@@ -92,7 +194,9 @@ function Graph(props: {dataBuffers: JsxElement}) {
             }}
           >
             <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
+            {/* <XAxis /> */}
             <YAxis stroke={theme.palette.text.secondary}>
+            {/* <YAxis> */}
               <Label
                 angle={270}
                 position="left"
@@ -117,9 +221,11 @@ function Graph(props: {dataBuffers: JsxElement}) {
 
 
 export default function CompositionApproach() {
+    const classes = useStyles();
     const [tags, setTags] = useState({
         "testTag123": 123
     });
+    const socket = io.connect('http://localhost:5001');
     const setRandomTags = () => {
         // var test : Record<string, number> = {"test": 1};
         var test : any = {"test": 1}; // FIXME find appropriate type
@@ -128,10 +234,15 @@ export default function CompositionApproach() {
         }
         setTags(test);
     }
-    return <>
-    <IconButton onClick={setRandomTags}>Generate New Tags</IconButton>
-    <ValueTable tags={tags}/>
-    </>
+    return <div className={classes.root}>
+        <main className={classes.content}>
+            <Container maxWidth="lg" className={classes.container}>
+                <IconButton onClick={setRandomTags}>Generate New Tags</IconButton>
+                <ValueTable tags={tags}/>
+                <BufferedGraph socket={socket} socketEventName="testEvent" lengths={10}/>
+            </Container>
+        </main>
+    </div>
 }
 
 
